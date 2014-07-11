ece4e2b03c08a4a06b2c42f56d99d03ef35b74e7
Jlit1001 <jordanlittell@yahoo.com>
2014-07-11 15:42:20 -0700

    fixed reference errors

--- game.js
+++ game.js
@@ -1,9 +1,71 @@
 //verify to see if browser supports canvas contenxt 
 //if so assign context to it
 var canvas = document.getElementById('canvas');
-if (canvas.getContext && canvas.getContext('2D')){
-	var ctx = canvas.getContext('2D');
+if (canvas.getContext && canvas.getContext('2d')){
+	var ctx = canvas.getContext('2d');
 }
 else {
 	console.log("Canvas not detected.");
 	}
+
+//create engine components 
+
+//our game consists of paddle, bricks, and ball
+//renderer, initialization
+
+(function(){
+//scope so as to avoid conflicts with global variables
+	var ctx = null;
+
+	var Game = {
+		canvas: document.getElementById("canvas"),
+		setup: function(){
+			if (canvas.getContext){
+				this.ctx = this.canvas.getContext('2d');
+				this.width = this.canvas.width;
+				this.height = this.canvas.height;
+				this.init();
+				Ctrl.init();
+			}
+		},
+		animate: function() {
+
+		},
+		draw: function(){
+			//clear rect will clear the drawing board each time update occurs
+			ctx.clearRect(0,0,this.width,this.height);
+			Background.draw();
+			Bricks.draw();
+			Paddle.draw();
+			Ball.draw();
+		},
+		init: function(){
+			Background.init();
+			Bricks.init();
+			Paddle.init();
+			Ball.init();
+		}	
+	}
+	var Background= {
+		init: function(){},
+		draw: function(){},
+	}
+	var Bricks= {
+		init: function(){},
+		draw: function(){},
+	}
+	var Paddle= {
+		init: function(){},
+		draw: function(){},
+	}
+	var Ball= {
+		init: function(){},
+		draw: function(){},
+	}
+	var Ctrl = {
+		init:function(){}
+	};
+	window.onload = function(){
+		Game.setup();
+	};
+}());
